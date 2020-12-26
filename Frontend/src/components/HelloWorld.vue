4b872a05a34c3618abd638afd9dfa1438b802608
seif7666 <seifeldin7666@gmail.com>
2020-12-26 17:54:21 +0200

    Remove script

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -19,19 +19,6 @@
     </div>
   </div>
 </template>
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-
-@Options({
-  props: {
-    msg: String
-  }
-})
-export default class HelloWorld extends Vue {
-  msg!: string;
-}
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   .btn1 {
