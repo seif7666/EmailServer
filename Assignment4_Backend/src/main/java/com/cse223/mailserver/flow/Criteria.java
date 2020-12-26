17dd83b0c89c178cb7ac1cfd133da33aa67e263a
seif7666 <seifeldin7666@gmail.com>
2020-12-26 21:59:23 +0200

    Delete Criteria.java

--- Assignment4_Backend/src/main/java/com/cse223/mailserver/flow/Criteria.java
+++
@@ -1,7 +0,0 @@
-package com.cse223.mailserver.flow;
-
-import java.util.ArrayList;
-
-public interface Criteria {
-    public ArrayList<? extends Message> filterCriteria (ArrayList<? extends Message> messages);
-}
